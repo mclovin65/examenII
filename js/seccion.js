617733392997c0ac9c63461935981dbf4095aaee
forellanabarrios@gmail.com <forellanabarrios@gmail.com>
2024-07-11 13:44:19 -0300

    seccion

---
+++ js/seccion.js
@@ -0,0 +1,8 @@
+let fogel=document.querySelector(".seccion");
+fogel.innerHTML=`
+<div class="seccion1">
+    <div class="rc1"></div>
+    <div class="rc2"></div>
+    <div class="rc3"></div>
+</div>
+`;
